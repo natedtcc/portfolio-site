77602ab330e91183c8ad32705c119bb327244a45
Nate Nasteff <nnasteff@protonmail.com>
2020-09-23 01:04:26 -0400

    lots of small changes, added 404 error page, minify css

---
+++ includes/404.php
@@ -0,0 +1,19 @@
+<?php
+
+$page_title = 'About | natenasteff.com |';
+
+include('header.html');
+
+echo '<!DOCTYPE html>
+<html>
+      <center>
+        <br /><br /><br /><br />
+        <h1
+          class="jumbotron"
+          style="color: white; background-color: transparent"
+        >
+          Ooops, sorry!
+        </h1>
+        <h3 class="display-5" style="color: white">This page doesnt exist.</h3>
+      </center>'; 
+?>
