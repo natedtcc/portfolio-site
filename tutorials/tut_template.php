c30321a8a77e2f559511ac24396d2a4a8155ddd3
Nate Nasteff <nnasteff@protonmail.com>
2020-10-16 20:28:57 -0400

    created template for tuts

---
+++ tutorials/tut_template.php
@@ -0,0 +1,29 @@
+<?php
+
+$page_title = 'Tutorials | natenasteff.com |';
+$current_page = 4;
+
+include(dirname(__FILE__) . '/../includes/header.html');
+
+echo '<div class="text-center">
+<div class="text-fade">
+  <div class="project-heading">
+    <h1 class="display-4">TITLE</h1>
+    <p class="lead"><br><br>
+      DESCRIPTION
+    </p>
+  </div>
+</div>
+
+<div class="container"><br>
+<p class="lead"><br>
+
+
+
+
+
+
+</div>
+
+
+';
